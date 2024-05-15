d90ac0f3559f8853c03f64eaeb343fbed15d4063
vijethkash123 <vijethkashyap035@gmail.com>
2024-05-15 23:14:34 +0530

    edit s

--- scratch 2.py
+++ scratch 2.py
@@ -1,49 +1,19 @@
-# from typing import List
+from typing import List
 
 
-# class Solution:
-#     def minOperations(self, nums: List[int], k: int) -> int:
-#         final_xor = 0
-#         # XOR of all integers in the array.
-#         for n in nums:
-#             final_xor = final_xor ^ n
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+        def dfs(i, subset):
+            if len(subset) > k or sum(subset) > n:
+                return
+            if subset and sum(subset) == n and len(subset) == k:
+                res.append(subset.copy())
+            else:
+                for num in range(i + 1, 10):
+                    dfs(num, subset + [num])
 
-#         count = 0
-#         # Keep iterating until both k and final_xor becomes zero.
-#         while k or final_xor:
-#             print(k, final_xor)
-#             # k % 2 returns the rightmost bit in k,
-#             # final_xor % 2 returns the rightmost bit in final_xor.
-#             # Increment counter, if the bits don't match.
-#             if (k % 2) != (final_xor % 2):
-#                 count += 1
-
-#             # Remove the last bit from both integers.
-#             # print(k//2, final_xor//2)
-#             k //= 2
-#             final_xor //= 2
-
-#         return count
-
-# print(Solution().minOperations(nums = [2,1,3,4], k = 1))
-
-
-
-
-# # rightshifting 4 -> 
-# # 4 in binary -> 0100
-# # Right shifting -> 010 -> gives 2
-# # Right shifting again -> 01 -> gives 1
-# # We do right shifting by floor division. Doing floor division on normal intergers is same as right shifting on binary numbers.
-
-# # Right shifting using right shift operator
-# # For binary Base 2:
-# # k = 0b100
-# # i = 0b001
-# # print(k>>1)  # 2
-# # print(k>>2)  # 1
-# # For integer/decimal Base 10:
-# # k = 4
-# # print(k>>1)  # 2
-
-print(2^1)
+        dfs(0, [])
+        return res
+    
+print(Solution().combinationSum3(k = 3, n = 7))
--- scratch3.py
+++ scratch3.py
@@ -1,64 +1,24 @@
-class Solution(object):
-    def min_window(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        # Struggled with this problem for a long while.
-        # Idea: Two pointers: moving end forward to find a valid window,
-        #                     moving start forward to find a smaller window
-        #                     counter and hash_map to determine if the window is valid or not
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        hamming_distance = 0
+        new = x ^ y
+        while new > 0:
+            new = new & (new-1)
+            hamming_distance += 1
+        return hamming_distance
+    
+# print(Solution().hammingDistance(7, 0))
 
-        # Count the frequencies for chars in t
-        hash_map = dict()
-        for c in t:
-            if c in hash_map:
-                hash_map[c] += 1
-            else:
-                hash_map[c] = 1
+# 1001 - 9
+# 1000 - 8
+# 0111 - 7 
+# 0110 - 6
+# 0101 - 5
+# 1011 - 11
+# 1010 - 10
 
-        start, end = 0, 0
-
-        # If the minimal length doesn't change, it means there's no valid window
-        min_window_length = len(s) + 1
-
-        # Start point of the minimal window
-        min_window_start = 0
-
-        # Works as a counter of how many chars still need to be included in a window
-        num_of_chars_to_be_included = len(t)
-
-        while end < len(s):
-            # If the current char is desired
-            if s[end] in hash_map:
-                # Then we decreased the counter, if this char is a "must-have" now, in a sense of critical value
-                if hash_map[s[end]] > 0:
-                    num_of_chars_to_be_included -= 1
-                # And we decrease the hash_map value
-                hash_map[s[end]] -= 1
-
-            # If the current window has all the desired chars
-            while num_of_chars_to_be_included == 0:
-                # See if this window is smaller
-                if end - start + 1 < min_window_length:
-                    min_window_length = end - start + 1
-                    min_window_start = start
-
-                # if s[start] is desired, we need to update the hash_map value and the counter
-                if s[start] in hash_map:
-                    hash_map[s[start]] += 1
-                    # Still, update the counter only if the current char is "critical"
-                    if hash_map[s[start]] > 0:
-                        num_of_chars_to_be_included += 1
-
-                # Move start forward to find a smaller window
-                start += 1
-
-            # Move end forward to find another valid window
-            end += 1
-
-        if min_window_length == len(s) + 1:
-            return ""
-        else:
-            return s[min_window_start:min_window_start + min_window_length]
+1111 - 15
+1110 - 14
+1101 - 13
+1100 - 12
+print(15 & 14)
