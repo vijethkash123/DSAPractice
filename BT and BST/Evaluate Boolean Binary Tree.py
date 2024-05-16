1ab7a2d7ed83eb59598b774649ea5197ed3d5e34
vijethkash123 <vijethkashyap035@gmail.com>
2024-05-17 00:26:52 +0530

    BT

--- BT and BST/Evaluate Boolean Binary Tree.py
+++ BT and BST/Evaluate Boolean Binary Tree.py
@@ -1,8 +1,7 @@
 # Leetcode - 2331
-# Definition for a binary tree node.
 from typing import Optional
 
-
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
