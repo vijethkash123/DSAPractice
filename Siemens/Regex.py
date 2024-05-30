37e27b8abba8d78a4b4877e0dfa95af892dcbef2
vijethkash123 <vijethkashyap035@gmail.com>
2024-05-31 01:45:26 +0530

    Union Find problems

--- Siemens/Regex.py
+++ Siemens/Regex.py
@@ -1,5 +1,6 @@
 import re
-s = "This is a sentence. this is a sentence"
+s = "This is a sentence. ;.;'.;'.+/*/-*-this is a;';';' sentence"
+print(s)
 s = re.sub(r'[^A-Za-z0-9 ]+', '', s)  # ^ says to replace anything other than A-Z, a-z, 0-9 and space
 print(s)
 
