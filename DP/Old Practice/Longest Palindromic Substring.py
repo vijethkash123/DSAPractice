e2db9c31dbaee07e84972d565e1b67d93be6915f
vijethkash123 <vijethkashyap035@gmail.com>
2024-07-31 23:42:48 +0530

    DP problems

---
+++ DP/Old Practice/Longest Palindromic Substring.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        
+        Max_Len=1
+        Max_Str=s[0]
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+            for j in range(i):
+                print(s[j:i+1], end = ", ")  # Printing the substring which are checked
+                if s[j] == s[i] and (i-j <= 2 or dp[j+1][i-1]):
+                    dp[j][i] = True
+                    if i-j+1 > Max_Len:
+                        Max_Len = i-j+1
+                        Max_Str = s[j:i+1]
+        return Max_Str
+
+print("\n" + "res: " + Solution().longestPalindrome(s = "racecar"))
+print("\n" + "res: " + Solution().longestPalindrome(s = "babad"))
