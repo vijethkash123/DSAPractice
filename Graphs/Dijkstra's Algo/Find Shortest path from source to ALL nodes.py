3a448d78319edd82c1100de3f0ce793ec5d9f173
vijethkash123 <vijethkashyap035@gmail.com>
2024-09-13 00:02:27 +0530

    refactor

--- Graphs/Dijkstra's Algo/Find Shortest path from source to ALL nodes.py
+++ Graphs/Dijkstra's Algo/Find Shortest path from source to ALL nodes.py
@@ -1,35 +1,7 @@
 from collections import defaultdict
 import heapq
 
-
-class Solution:
-
-    # Function to find the shortest distance of all the vertices from the source vertex S.
-    def dijkstra(V, adj, S):
-        dist = [float("inf") for i in range(V)]
-        minHeap = [[0, S]]  # S is source vertex
-        heapq.heapify(minHeap)  # [weight, edge]
-        while minHeap:
-            w1, n1 = heapq.heappop(minHeap)
-            if dist[n1] != float("inf"):   # visited
-                continue
-            dist[n1] = w1  # adding distance
-            for w2, n2 in adj[n1]:
-                if dist[n2] == float("inf"):  # unvisited
-                    heapq.heappush(minHeap, [w1 + w2, n2])
-        
-        for i in range(V):
-            if dist[i] == float("inf"):  # unreachble to graph/ disconnected node
-                dist[i] = -1
-        return dist
-
-print(Solution.dijkstra(V = 3, adj = {0: [[1, 1], [6, 2]], 1: [[3, 2], [1, 0]], 2: [[3, 1], [6, 0]]}, S = 2))  # undirected
-print(Solution.dijkstra(V = 3, adj = {0: [[4, 1], [1, 2]], 1: [] , 2: [[2, 1]]}, S = 0))   # directed
-print(Solution.dijkstra(V = 4, adj = {0: [[7, 1], [4, 2]], 1: [] , 2: [[4, 1], [1, 3]], 3:[[1, 1]]}, S = 0))  # directed
-
-
-
-# Optimized version
+# Optimized version - Read this as template
 class Solution:
 
     # Function to find the shortest distance of all the vertices from the source vertex S.
@@ -55,6 +27,35 @@ class Solution:
                 if newDist < dist[n2]:
                     dist[n2] = newDist
                     heapq.heappush(minHeap, [newDist, n2])
+        
+        for i in range(V):
+            if dist[i] == float("inf"):  # unreachble to graph/ disconnected node
+                dist[i] = -1
+        return dist
+
+print(Solution.dijkstra(V = 3, adj = {0: [[1, 1], [6, 2]], 1: [[3, 2], [1, 0]], 2: [[3, 1], [6, 0]]}, S = 2))  # undirected
+print(Solution.dijkstra(V = 3, adj = {0: [[4, 1], [1, 2]], 1: [] , 2: [[2, 1]]}, S = 0))   # directed
+print(Solution.dijkstra(V = 4, adj = {0: [[7, 1], [4, 2]], 1: [] , 2: [[4, 1], [1, 3]], 3:[[1, 1]]}, S = 0))  # directed
+
+
+
+
+# Another verion added for reference.
+class Solution:
+
+    # Function to find the shortest distance of all the vertices from the source vertex S.
+    def dijkstra(V, adj, S):
+        dist = [float("inf") for i in range(V)]
+        minHeap = [[0, S]]  # S is source vertex
+        heapq.heapify(minHeap)  # [weight, edge]
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            if dist[n1] != float("inf"):   # visited
+                continue
+            dist[n1] = w1  # adding distance
+            for w2, n2 in adj[n1]:
+                if dist[n2] == float("inf"):  # unvisited
+                    heapq.heappush(minHeap, [w1 + w2, n2])
         
         for i in range(V):
             if dist[i] == float("inf"):  # unreachble to graph/ disconnected node
