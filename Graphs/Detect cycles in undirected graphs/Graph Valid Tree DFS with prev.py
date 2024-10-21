0f2e7fc3b73b555d7f6f923be475c20e08be4e0e
vijethkash123 <vijethkashyap035@gmail.com>
2024-10-21 18:01:05 +0530

    Update graphs folder

--- Graphs/Detect cycles in undirected graphs/Graph Valid Tree DFS with prev.py
+++ Graphs/Detect cycles in undirected graphs/Graph Valid Tree DFS with prev.py
@@ -9,22 +9,24 @@ class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         visited = set()
         adj = defaultdict(list)
+        res = True
 
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)  # Undirected, so add both directions
         
         def dfs(node, prev):
-            nonlocal visited
-            if node in visited :
-                return False
+            nonlocal visited, res
+            if node in visited:
+                res = False
+                return
             visited.add(node)
             for nei in adj[node]:
-                if nei != prev and not dfs(nei, node):  # sending prev value as node is very important concept so that we don't detect false positive cycle in the graph
-                    return False
-            return True
+                if nei != prev:
+                    dfs(nei, node):  # sending prev value as node is very important concept so that we don't detect false positive cycle in the graph
+            return
 
-        res = dfs(0, -1)  
+        dfs(0, -1)  
         if len(visited) != n or not res:
             return False
         return True
